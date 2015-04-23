e1d54d9620ddfe41b6f2af9322ef41ebde5fcb14
Nathan Fixler <nathan@fixler.org>
2015-04-23 09:14:57 -0700

    Switch Hipchat consumer to incidents endpoint
    The status indicator HipChat is posting on statuspage.io isn't very
    useful for identifying the severity of the status update. I switched to
    pulling the full incident history for more accurate messages.

--- lib/checkups/consumers/hipchat_consumer.rb
+++ lib/checkups/consumers/hipchat_consumer.rb
@@ -1,7 +1,12 @@
 module Checkups
   module Consumers
     class HipchatConsumer < StatusPageConsumer
-      STATUS_URL = 'https://status.hipchat.com/api/v1/status.json'
+      STATUS_URL = 'https://status.hipchat.com/api/v1/incidents.json'
+
+      def consume
+        msg = open(STATUS_URL).read
+        new_status(JSON.parse(msg)['incidents'].first)
+      end
     end
   end
 end
--- lib/checkups/statuses/hipchat_status.rb
+++ lib/checkups/statuses/hipchat_status.rb
@@ -2,6 +2,35 @@ module Checkups
   module Statuses
     class HipchatStatus < Status
       SENDER = 'HipChat'
+
+      IMPACT_COLORS = {
+        'major' => 'red',
+        'minor' => 'yellow',
+        'none' => 'yellow'
+      }
+
+      def body
+        "#{name}: #{last_update['body']}"
+      end
+
+      def color
+        return 'green' if resolved?
+        IMPACT_COLORS.fetch(impact, 'yellow')
+      end
+
+      private
+
+      def last_update
+        incident_updates.first
+      end
+
+      def resolved?
+        status == 'resolved'
+      end
+
+      def status
+        last_update['status']
+      end
     end
   end
 end
