ee2739f27a8cb5ef93e1a5c670f65b455d86344a
Nathan Fixler <nathan@fixler.org>
2017-03-28 16:56:46 -0500

    Update dnssimple.com status page URL

--- lib/checkups/consumers/dnsimple_consumer.rb
+++ lib/checkups/consumers/dnsimple_consumer.rb
@@ -1,7 +1,7 @@
 module Checkups
   module Consumers
     class DnsimpleConsumer < StatusPageConsumer
-      STATUS_URL = 'http://dnsimplestatus.com/api/v1/status.json'
+      STATUS_URL = 'https://dnsimple.statuspage.io/api/v1/status.json'
     end
   end
 end
